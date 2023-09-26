8edc8e182e516e813319bf1d0c9517b607659cdc
ahmadjamaljallab <ahm0189567@ju.edu.jo>
2023-09-26 21:54:22 +0300

    login_page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import {TextField ,Button} from "@mui/material";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+        <h1>Hello Amplified React!</h1>
+        <div className="Flex_container ">
+            <TextField id="standard-basic" label="username" variant="standard" />
+            <TextField id="filled-basic" label="password" variant="standard" />
+            <Button variant="contained">login</Button>
+        </div>
     </div>
   );
 }
